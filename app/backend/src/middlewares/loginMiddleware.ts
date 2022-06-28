afe68a0454857b87cce19321585c9eceef760ac7
juan victor <juanvictor70@gmail.com>
2022-06-28 12:46:40 -0400

    criando middlewares para o login

---
+++ app/backend/src/middlewares/loginMiddleware.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from 'express';
+
+export default class LoginMiddleware {
+  static checkInfo(req: Request, res: Response, next: NextFunction) {
+    const info = req.body;
+    if (!info.email) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    if (!info.password) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    next();
+  }
+}
--- app/backend/src/routers/loginRouter.ts
+++ app/backend/src/routers/loginRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import LoginController from '../database/controllers/loginController';
+import LoginMiddleware from '../middlewares/loginMiddleware';
 
 const router = Router();
 
 router.post('./login', LoginController.postLogin);
+router.post('/login', LoginMiddleware.checkInfo, LoginController.postLogin);
 
 export default router;
